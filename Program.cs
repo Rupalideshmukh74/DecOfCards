2ebd60e5d5fffca58c843818be8a1c30b12ccbde
Rupalideshmukh74 <deshmukhrupali39@gmail.com>
2021-08-19 21:27:11 +0530

    [Rupali]feat:welcome message

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Dec_of_cards
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Dec of cards!");
+            Cards cards = new Cards();
+            cards.CardsCreator();
+        }
+    }
+}
